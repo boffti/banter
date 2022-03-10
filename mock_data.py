dae46aa15653751a77411fc8a6a414071bf98777
Aneesh Melkot <boffti@gmail.com>
2022-03-10 14:20:10 -0600

    Updated billboard

--- mock_data.py
+++ mock_data.py
@@ -4,6 +4,7 @@ billboard = [
         "title": "Library exhibit celebrates 50 years of Conhi",
         "content": "In honor of the College of Nursing and Health Innovation’s (CONHI) 50TH anniversary, Special Collections is hosting the new exhibit, A Tribute to Nurses: UTA and Beyond. It is on display now and open for visitors from 9 a.m. to 5 p.m. Tuesday-Saturday through Sept. 15.",
         "author": "Neph Rivera",
+        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/pickard-with-students_zso5hd.jpg",
         "date_posted": "2019-01-01"
     },
     {
@@ -11,6 +12,7 @@ billboard = [
         "title": "UTA opens center for rural health and nursing",
         "content": "The College of Nursing and Health Innovation (CONHI) at The University of Texas at Arlington has launched a new center to enhance access to health care for Texas’ rural communities. The Center for Rural Health and Nursing is funded by a $4 million legislative appropriation by the state of Texas. The funding will support the centers efforts to improve rural nursing education and the health outcomes of rural populations.",
         "author": "Neph Rivera",
+        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/center-rural-health-leadership_kgb0q5.jpg",
         "date_posted": "2019-01-02"
     },
     {
@@ -18,15 +20,17 @@ billboard = [
         "title": "Women's history month offers change to learn and reflect",
         "content": "On International Womens Day, Tuesday March 8, visit the UTA Multicultural Affairs Womens History Month display with staff and students to learn more about the holiday and different activists and their efforts for rights around the globe. The booth will be on display in the University Center by the cafeteria entrance starting at 11 a.m.",
         "author": "Devynn Case",
+        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/womens_history_month_logo_aymaxx.png",
         "date_posted": "2019-01-03"
     },
-    {
-        "id": 4,
-        "title": "Bioengineering professor honored for scientific contributions.",
-        "content": "The International Society for Optics and Photonics (SPIE) has named University of Texas at Arlington researcher Baohong Yuan a fellow. SPIE recognized Yuan, a professor in the Bioengineering Department, for outstanding contributions to developing high-resolution fluorescence imaging technologies in deep tissue.",
-        "author": "Herb Booth",
-        "date_posted": "2019-01-04"
-    },
+    # {
+    #     "id": 4,
+    #     "title": "Bioengineering professor honored for scientific contributions.",
+    #     "content": "The International Society for Optics and Photonics (SPIE) has named University of Texas at Arlington researcher Baohong Yuan a fellow. SPIE recognized Yuan, a professor in the Bioengineering Department, for outstanding contributions to developing high-resolution fluorescence imaging technologies in deep tissue.",
+    #     "author": "Herb Booth",
+    #     "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/baohong-yuan-portrait-new_gwnezy.jpg",
+    #     "date_posted": "2019-01-04"
+    # },
     # {
     #     "id": 5,
     #     "title": "UTA to test Dallas roads, evaluate the remaining service life",
@@ -48,13 +52,14 @@ billboard = [
     #     "author": "Herb Booth",
     #     "date_posted": "2019-01-07"
     # },
-    # {
-    #     "id": 8,
-    #     "title": "UTA engineers named to Nataional Academy of Inventors",
-    #     "content": "The National Academy of Inventors (NAI) has elected two engineers from The University of Texas at Arlington as senior members of the organization. NAI senior members are active faculty, scientists and administrators who have demonstrated remarkable innovation by producing technologies that have the potential to significantly impact the welfare of society.",
-    #     "author": "Linsey Retcofsky",
-    #     "date_posted": "2019-01-08"
-    # },
+    {
+        "id": 8,
+        "title": "UTA engineers named to Nataional Academy of Inventors",
+        "content": "The National Academy of Inventors (NAI) has elected two engineers from The University of Texas at Arlington as senior members of the organization. NAI senior members are active faculty, scientists and administrators who have demonstrated remarkable innovation by producing technologies that have the potential to significantly impact the welfare of society.",
+        "author": "Linsey Retcofsky",
+        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943558/banter/billboard/hong-wijesundara-thumbnail_hhmmno.jpg",
+        "date_posted": "2019-01-08"
+    },
 ]
 
 events = [
