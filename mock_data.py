b4759c29e3a05cf9c343c9b69c9f1cb4cdb12943
Aneesh Melkot <boffti@gmail.com>
2022-03-10 19:12:15 -0600

    Competed Home Page

--- mock_data.py
+++ mock_data.py
@@ -4,7 +4,7 @@ billboard = [
         "title": "Library exhibit celebrates 50 years of Conhi",
         "content": "In honor of the College of Nursing and Health Innovation’s (CONHI) 50TH anniversary, Special Collections is hosting the new exhibit, A Tribute to Nurses: UTA and Beyond. It is on display now and open for visitors from 9 a.m. to 5 p.m. Tuesday-Saturday through Sept. 15.",
         "author": "Neph Rivera",
-        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/pickard-with-students_zso5hd.jpg",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/pickard-with-students_zso5hd.jpg",
         "date_posted": "2019-01-01"
     },
     {
@@ -12,7 +12,7 @@ billboard = [
         "title": "UTA opens center for rural health and nursing",
         "content": "The College of Nursing and Health Innovation (CONHI) at The University of Texas at Arlington has launched a new center to enhance access to health care for Texas’ rural communities. The Center for Rural Health and Nursing is funded by a $4 million legislative appropriation by the state of Texas. The funding will support the centers efforts to improve rural nursing education and the health outcomes of rural populations.",
         "author": "Neph Rivera",
-        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/center-rural-health-leadership_kgb0q5.jpg",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/center-rural-health-leadership_kgb0q5.jpg",
         "date_posted": "2019-01-02"
     },
     {
@@ -20,7 +20,7 @@ billboard = [
         "title": "Women's history month offers change to learn and reflect",
         "content": "On International Womens Day, Tuesday March 8, visit the UTA Multicultural Affairs Womens History Month display with staff and students to learn more about the holiday and different activists and their efforts for rights around the globe. The booth will be on display in the University Center by the cafeteria entrance starting at 11 a.m.",
         "author": "Devynn Case",
-        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/womens_history_month_logo_aymaxx.png",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943323/banter/billboard/womens_history_month_logo_aymaxx.png",
         "date_posted": "2019-01-03"
     },
     # {
@@ -36,7 +36,7 @@ billboard = [
         "title": "UTA to test Dallas roads, evaluate the remaining service life",
         "content": "A University of Texas at Arlington civil engineering professor is leading a $949,000 project with the city of Dallas to evaluate its roads and determine remaining service life. Ali Abolmaali, chair of the Civil Engineering Department and the Tseng Huang Endowed Professor of structural and applied mechanics, said he and his team will evaluate three test pavement strips in the city of Dallas.",
         "author": "Herb Booth",
-        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646954123/banter/billboard/apf-old-img1_e8nwgt.jpg",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646954123/banter/billboard/apf-old-img1_e8nwgt.jpg",
         "date_posted": "2019-01-05"
     },
     {
@@ -59,7 +59,7 @@ billboard = [
         "title": "UTA engineers named to Nataional Academy of Inventors",
         "content": "The National Academy of Inventors (NAI) has elected two engineers from The University of Texas at Arlington as senior members of the organization. NAI senior members are active faculty, scientists and administrators who have demonstrated remarkable innovation by producing technologies that have the potential to significantly impact the welfare of society.",
         "author": "Linsey Retcofsky",
-        "img_url":"https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943558/banter/billboard/hong-wijesundara-thumbnail_hhmmno.jpg",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646943558/banter/billboard/hong-wijesundara-thumbnail_hhmmno.jpg",
         "date_posted": "2019-01-08"
     },
 ]
@@ -95,34 +95,52 @@ events = [
     {
         "id": 4,
         "title": "Free HIV Testing Day in the Health Center",
-        "description": "Event 4 content",
+        "description": "Come and get tested for free. The Health Center is hosting a free HIV testing day on Tuesday, March 8, 2019. The Health Center is located at the...",
         "date_time": "2019-01-04 12:00:00",
         "location": "Health Center",
         "author": "Event 4 author",
         "date_posted": "2019-01-04"
+    },
+    {
+        "id": 1,
+        "title": "Mel Chin: Inescapable Histories Exhibition",
+        "description": "Art exhibition featuring 24 works by Mel Chin, a world-renowned conceptual artist who utilizes painting, sculpture, film, drawing, collage, as well as other...",
+        "date_time": "2019-01-01 12:00:00",
+        "location": "Gallery at UTA, The, 169 Fine Arts Building",
+        "author": "Event 1 author",
+        "date_posted": "2019-01-01"
+    },
+    {
+        "id": 2,
+        "title": "'Women's Health, Women's Bodies' Exhibit",
+        "description": "In celebration of Women's History Month, the UTA Libraries Special Collections has partnered with the UTA Women's and Gender Studies Program to bring you...",
+        "date_time": "2019-01-02 12:00:00",
+        "location": "6th Floor, Central Library",
+        "author": "Event 2 author",
+        "date_posted": "2019-01-02"
     },
 ]
 
 clubs = [
     {
         "id": 1,
-        "name": "Club 1",
-        "description": "Club 1 content",
-        "img_url":"",
-        "school_id":1
+        "name": "Accounting Society",
+        "description": "Welcome to the Accounting Society Homepage.  We are a professional student organization within the College of Business that provides students with the opportunities to meet recruiters/professionals in the field of Accounting. Within Accounting Society, students have the opportunity to build relationships between UTA’s accounting alumni and other professional firms and individuals in the Dallas-Fort Worth metroplex. We invite you to look at our page and join in on the experience the society creates.",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646960591/banter/clubs/381c1232-04ad-4481-9f4a-8bed254e88e1049c17df-795b-4f70-994a-dcd34ec7d064_xi746w.jpg",
+        "school_id": 1
     },
     {
         "id": 2,
-        "name": "Club 2",
-        "description": "Club 2 content",
-        "img_url":"",
-        "school_id":1
+        "name": "American Institute of Graphic Arts",
+        "description": "The AIGA UTA Student Group is a student run affiliate of the national American Institute of Graphic Arts. Our group aims to enhance the education of UTA design students by offering workshops, resources, critiques, and holding events to strengthen the design community. Members learn to take an initiative in their design goals and gain a better professional worldview.",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646960787/banter/clubs/6e6d9959-dd3b-4bda-8761-a96855c288d3e6f7cf11-d264-4c4d-8ccc-08f65034c11e_zwiqzf.jpg",
+        "school_id": 1
     },
     {
         "id": 3,
-        "name": "Club 3",
-        "description": "Club 3 content",
-        "img_url":"",
-        "school_id":1
+        "name": "Badminton - Sport Club",
+        "description": "Whether you just want to have fun, love meeting up with people, or want professional training, you are welcomed to join us during our practice hours listed below.",
+        "img_url": "https://res.cloudinary.com/dtvhyzofv/image/upload/v1646960838/banter/clubs/6b0e0337-e3b3-4144-bb0d-3143337909b01eb6d7cc-f08a-4785-a339-471517f75ede_pfl8oh.jpg",
+        "school_id": 1
     },
 ]
